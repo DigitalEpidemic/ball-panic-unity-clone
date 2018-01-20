23e6bfcdaa32c6582396017eaa695673ec5f6a39
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-20 01:21:01 -0500

    Added arrow movement

---
+++ Assets/Scripts/Arrow/ArrowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+	private float arrowSpeed = 4.0f;
+	private bool canShootStickyArrow;
+
+	// Use this for initialization
+	void Start () {
+		canShootStickyArrow = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShootArrow ();
+	}
+
+	void ShootArrow () {
+		Vector3 temp = transform.position;
+		temp.y += arrowSpeed * Time.unscaledDeltaTime;
+		transform.position = temp;
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "TopBrick") {
+			gameObject.SetActive (false);
+		}
+	}
+
+} // ArrowScript
